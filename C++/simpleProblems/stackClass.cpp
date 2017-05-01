a2d4729fd15e7b862ea3eb0f0d2dad241b6256f6
David Jiang <davidjiang@hp.com>
2017-05-01 14:00:01 -0700

    new structure

---
+++ C++/simpleProblems/stackClass.cpp
@@ -0,0 +1,24 @@
+/*
+ * Simple stack implemention using a fixed array class
+ */
+
+#include <iostream>
+
+class myStack
+{
+	int myArray[10];
+	int curLength;
+	public:
+		void reset(){};
+		void push(){};
+		void pop(){};
+		void print(){};
+};
+
+
+int main()
+{
+	
+	
+	return 0;
+}
