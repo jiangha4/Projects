b1cba9c3532309570bb781c681693ec08d747263
David Jiang <davidjiang.haohan@gmail.com>
2017-07-05 17:49:01 -0700

    renamed array file

---
+++ C++/dataStructures/dynamicArrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Goal: Implement a vector using a class 
+ * Design: 
+ * 	1. Automatic resizing will be done when array is at 75% capacity and space will be doubled
+ * 	2. Space will be halfed when at 40% capacity 
+ */
+
+class myVector{
+	int size; //Stores current size of the vector
+	int capacity; //Stores current capacity of the vector
+	int *head; //Point to the head of the vector	
+
+	public:
+		int size();
+		int capacity();
+		void insert(int);
+};
+
+int main(){
+	myVector testVector();
+
+	return 0;
+}
+
