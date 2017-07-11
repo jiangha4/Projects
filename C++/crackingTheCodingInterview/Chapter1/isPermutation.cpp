e292999b5170aee0543f0d13db8cf6245a12d9cc
David Jiang <davidjiang.haohan@gmail.com>
2017-07-10 17:14:42 -0700

    question 2

---
+++ C++/crackingTheCodingInterview/Chapter1/isPermutation.cpp
@@ -0,0 +1,30 @@
+/*
+ * Question 2: Given two strings, write a method to decide if one is a permutation
+ * of the other.
+ */
+
+#include <string>
+#include <iostream>
+#include <iterator>
+#include <set>
+
+using namespace std;
+
+//Approach: Use a map and store the occurance of characters in str1. Compare
+//to str2.
+
+bool isPermutation(string str1, string str2)
+{
+	set<string> isPerm;
+	pair<str<string> iterator, bool> result;
+
+	for(string::iterator it=str1.begin(); it != str1.end(); it++){
+
+	}
+}
+
+int main()
+{
+	/* code */
+	return 0;
+}
