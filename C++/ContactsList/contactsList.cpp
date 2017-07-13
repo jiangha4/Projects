543daae57bf32405afaa539944508e7bcb5fcf35
David Jiang <davidjiang.haohan@gmail.com>
2017-07-12 17:53:25 -0700

    contact list

--- C++/ContactsList/contactsList.cpp
+++ C++/ContactsList/contactsList.cpp
@@ -1,8 +1,14 @@
 /*
- * Assignment: Create a phone contact list
- * Functions needed: Add, Delete, Search, Total, Print
+ * Project: Create a phone contact list.
+ * Basic functions needed: Add, Delete, Search, Total, Print
+ * Should haves:
+ * 	- Ability to read first names, last names, and numbers from a text file and construct a contact book.
+ * 	- Ability to write all the names to a output file in alphabetical order
+ * 	- Ability to search by first or last name and return all matching contacts
+ * 	- Ability to search by phone number and return matching contact
  */
 
+#include <algorithm>
 #include <string>
 #include <cstddef>
 #include <iostream>
@@ -10,18 +16,8 @@
 #include <iterator>
 
 /*
- * This function is used to turn all input names in contactPerson to be in lower case.
- * Helps with search to ensure that "Alan" can be found with "alan"
- */
-void lowerCaseAllElements(std::string &str){
-	for(auto c : str){
-		c = tolower(c);
-	}
-}
-
-/*
  * Class contactPerson is a set of strings that contains the contacts first name,
- * last name, and phone number. This class can also edit the individual values of 
+ * last name, and phone number. This class can also edit the individual values of
  * each contact after initialization.
  */
 
@@ -32,13 +28,13 @@ class contactPerson{
 		contactPerson(std::string first, std::string last)
 		: firstName(first), lastName(last) {};
 		contactPerson(std::string first, std::string last, std::string number)
-		: firstName(first), lastName(last), phoneNum(number) {}; 
-	
+		: firstName(first), lastName(last), phoneNum(number) {};
+
 		std::ostream &print(std::ostream &os){
 			os << this->firstName << "\n"  << this->lastName << "\n" << this->phoneNum << std::endl;
 			return os;
 		};
-	
+
 		std::string getFirstName(){
 			return this->firstName;
 		};
@@ -46,13 +42,13 @@ class contactPerson{
 		 std::string getLastName(){
 			return this->lastName;
 		};
-		
+
 		std::string getPhoneNumber(){
 			return this->phoneNum;
 		};
-	
+
 	private:
-		std::string firstName, lastName, phoneNum;	
+		std::string firstName, lastName, phoneNum;
 };
 
 /*
@@ -61,7 +57,7 @@ class contactPerson{
 class contactBookVector{
 	public:
 		contactBookVector() = default;
-		
+
 		contactBookVector &add(contactPerson &person){
 			contactList.push_back(person);
 			return *this;
@@ -70,7 +66,7 @@ class contactBookVector{
 		std::vector<contactPerson>::size_type total(){
 			return this->contactList.size();
 		};
-		
+
 		std::ostream &print(std::ostream &os){
 			std::vector<contactPerson>::iterator it = this->contactList.begin();
 			for(it; it != this->contactList.end(); ++it){
@@ -78,48 +74,11 @@ class contactBookVector{
 			}
 			return os;
 		};
-		
-		/*
- 		* A brute force search implemention
- 		* Relys on iterating through the entire vector contactList
- 		*/
-		std::vector<contactPerson> searchByName(const std::string &first, const std::string &last = std::string()){
-			std::vector<contactPerson> contacts;
-			for(auto person : this->contactList){
-				if(last.empty()){
-					if(person.getFirstName() == first){
-						contacts.push_back(person);
-					}
-				}else{
-					if(person.getFirstName() == first && person.getLastName() == last){
-						contacts.push_back(person);
-					} 
-				}
-			}
-			return contacts;
-		}
-		
+
 	private:
 		std::vector<contactPerson> contactList;
 };
 
 int main(){
-	contactBookVector book1;
-
-	std::string a = "blah"; 
-
-	contactPerson person1(a, a);
-
-	std::string b = "world";
-	contactPerson person2(b, b);
-
-	book1.add(person2);
-	book1.add(person1);
-	
-	std::vector<contactPerson> names = book1.searchByName(a, a);
-	for(auto a: names){
-		a.print(std::cout);
-	}
-
 	return 0;
 }
